c680650362f8f3b31c919bda94371b3498aae7ef
Shruti <shrutisablaniya93@gmail.com>
2021-09-29 17:37:43 +0530

    Delete last element from linked list

---
+++ UC6_DeleteLastElement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructurePrograms
+{
+    class UC6_DeleteLastElement
+    {
+        public Node head;
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+                next = null;
+            }
+        }
+        public Node lastElement()
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return null;
+            Node newNode = head;
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
+
+        public void Append(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        public void printList()
+        {
+            Node nodes = head;
+            Console.Write("\n\nLinked List:  ");
+            while (nodes != null)
+            {
+                Console.Write(nodes.data + " ");
+                nodes = nodes.next;
+            }
+        }
+    }
+}
